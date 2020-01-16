7e8e27c18c9d49cd80b9c3fb78885d63f7fbee88
analuciablanco <analuciablanco@outlook.com>
2020-01-15 19:58:29 -0700

    primer script

---
+++ Assets/Scripts/Character2D.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character2D : MonoBehaviour
+{
+    void Update()
+    {
+        //transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+    }
+}
