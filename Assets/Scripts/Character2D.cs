052a116b03caf4746959be95297a0b383ff5836b
analuciablanco <analuciablanco@outlook.com>
2020-01-16 07:54:17 -0700

    funcion de axis y colisiones

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour
 {
+
+    // public permite que sea visible la variable en el Inspector de Unity.
+    // Cabeceras serializables: una variable, aun que sea privada, es visible en el Inspector.
+
+    [SerializeField] float moveSpeed = 5f;
+
+    // Se declara de esta manera preferentemente cuando no se quiere hacer visible en el Inspector.
+    //float moveSpeed{get; set;}
+    // Si se quiere mostrar en el Inspector, se declara de la siguiente manera:
+    //public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+
+    // Método.
     void Update()
     {
-        //transform.Translate(Vector2.right * 2f * Time.deltaTime);
-        transform.Translate(Vector2.right * 2f * Time.deltaTime);
+        // "Axis": es para recibir valores de teclado.
+        // "moveSpeed": velocidad del movimiento (modificable en el Inspector).
+        transform.Translate(Axis * moveSpeed * Time.deltaTime);
     }
+
+    // Funcion: método con return.
+    Vector2 Axis 
+    {
+        // get: devuelve
+        // =>: indica lo que va a encapsular
+        get => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+    }
+
+    // Hace lo mismo que el anterior, tambien es una función.
+    /* Vector2 Axis2()
+    {
+        return new Vector2();
+    } */
+
+    // Se declara así para que se muestre en el Inspector.
+    public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
 }
