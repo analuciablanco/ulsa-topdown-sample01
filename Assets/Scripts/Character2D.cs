8284474c26ff31a765dfa4a54824b6bfb45930e6
analuciablanco <analuciablanco@outlook.com>
2020-01-20 00:30:13 -0700

    Animaciones, estados y movimiento de personaje

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class Character2D : MonoBehaviour
 {
 
+    Animator anim;
+    SpriteRenderer spr;
+    Rigidbody2D rb2d;
+
     // public permite que sea visible la variable en el Inspector de Unity.
     // Cabeceras serializables: una variable, aun que sea privada, es visible en el Inspector.
 
-    [SerializeField] float moveSpeed = 5f;
+    [SerializeField, Range(0.1f, 20f)] float moveSpeed = 5f;
 
     // Se declara de esta manera preferentemente cuando no se quiere hacer visible en el Inspector.
     //float moveSpeed{get; set;}
     // Si se quiere mostrar en el Inspector, se declara de la siguiente manera:
     //public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+
+    [SerializeField] Color rayColor = Color.magenta;
+    [SerializeField, Range(0.1f, 20f)] float rayDistance = 5f;
+    [SerializeField] LayerMask groundLayer;
+
+    [SerializeField, Range(0.1f, 20f)] float jumpForce = 7f;
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        spr = GetComponent<SpriteRenderer>();
+        rb2d = GetComponent<Rigidbody2D>();
+    }
 
     // Método.
     void Update()
@@ -21,6 +38,25 @@ public class Character2D : MonoBehaviour
         // "Axis": es para recibir valores de teclado.
         // "moveSpeed": velocidad del movimiento (modificable en el Inspector).
         transform.Translate(Axis * moveSpeed * Time.deltaTime);
+        spr.flipX = Flip;
+    }
+
+    void FixedUpdate()
+    {
+        if(Grounding)
+        {
+            if(JumpButton)
+            {
+                rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                anim.SetTrigger("jump");
+            }   
+        }
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("moveX", Mathf.Abs(Axis.x));
+        anim.SetBool("landing", Grounding);
     }
 
     // Funcion: método con return.
@@ -39,4 +75,25 @@ public class Character2D : MonoBehaviour
 
     // Se declara así para que se muestre en el Inspector.
     public float MoveSpeed { get => moveSpeed; set => moveSpeed = value; }
+
+    bool Flip
+    {
+        get => Axis.x > 0f ? false : Axis.x < 0f ? true : spr.flipX;
+    }
+
+    bool Grounding
+    {
+        get => Physics2D.Raycast(transform.position, Vector2.down, rayDistance, groundLayer);
+    }
+
+    bool JumpButton
+    {
+        get => Input.GetButtonDown("Jump");
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = rayColor;
+        Gizmos.DrawRay(transform.position, Vector2.down * rayDistance);
+    }
 }
